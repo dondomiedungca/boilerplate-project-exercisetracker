e1a61ce137d7a995fa4366b7f6e65ca30e27dfaa
Dondomie Dungca <ddungca@isbx.com>
2024-11-04 21:48:25 +0800

    Initial commit

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const datePattern = /^\d{4}-\d{2}-\d{2}$/;
-const url =
-  "mongodb+srv://ddungca:password123!A@cluster0.ayhibtm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url = "";
 const client = new MongoClient(url);
 const db = client.db("sample_mflix");
 const users = "users";
